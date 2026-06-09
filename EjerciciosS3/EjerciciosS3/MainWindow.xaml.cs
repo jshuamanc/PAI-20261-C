5103b2cec50a4e5ecedf33172da6529550d5e947
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-06-09 14:56:56 -0500

    Version Inicial

---
+++ EjerciciosS3/EjerciciosS3/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjerciciosS3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAplicar_Click(object sender, RoutedEventArgs e)
+        {
+            lbTexto.FontFamily = new FontFamily("Segoe UI");
+            lbTexto.Foreground = Brushes.Black;
+            lbTexto.Background = Brushes.Transparent;
+
+            if (cbTipoLetra.IsChecked == true)
+            {
+                lbTexto.FontFamily = new FontFamily("Consolas");
+            }
+            if(cbColorTexto.IsChecked == true) {
+                lbTexto.Foreground = Brushes.Blue;
+            }
+            if(cbColorFondo.IsChecked == true) { 
+                lbTexto.Background = Brushes.Aqua;
+            }
+        }
+    }
+}
