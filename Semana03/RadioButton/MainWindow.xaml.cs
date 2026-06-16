5d52bf46c8f7b4b05eb7cf7cf2961ab7313714db
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-06-15 19:36:54 -0500

    semana 03

---
+++ Semana03/RadioButton/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroA = Int32.Parse(txtNumeroA.Text);
+            int numeroB = Int32.Parse(txtNumeroB.Text);
+            double total = 0;
+            if (rbSuma.IsChecked == true)
+            {
+                total = numeroA + numeroB;
+            }
+            else if (rbResta.IsChecked == true)
+            {
+                total = numeroA - numeroB;
+            }
+            else if(rbMultiplicacion.IsChecked == true)
+            {
+                total = numeroA * numeroB;
+            }
+            else if(rbDivision.IsChecked == true)
+            {
+                total = Convert.ToDouble(numeroA) / Convert.ToDouble(numeroB);
+            }
+            lbResultado.Content = $"Resultado: {Math.Round(total,2)}";
+        }
+    }
+}
---
+++ Semana03/Semana03/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semana03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtCantidad.Text.Length == 0)
+            {
+                MessageBox.Show("La cantidad no puede ser nulo");
+                return;
+            }
+            int cantidad = Int32.Parse(txtCantidad.Text);
+            decimal total = 0;
+            if(cbCebolla.IsChecked == true )
+            {
+                total += cantidad * 3;
+            }
+            if (cbTomate.IsChecked == true) {
+                total += cantidad * 1.5m;
+            }
+            if (cbPapa.IsChecked == true)
+            {
+                total += cantidad * 2;
+            }
+
+            lbTotal.Content = $"Total S/.: {total}";
+        }
+    }
+}
