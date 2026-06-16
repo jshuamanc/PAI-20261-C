bd85477e3e9c86ad2d31915241610e83963d682f
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-06-15 19:29:41 -0500

    Semana 04

---
+++ Semana04/Semana04/ListView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana04
+{
+    /// <summary>
+    /// Lógica de interacción para ListView.xaml
+    /// </summary>
+    public partial class ListView : Window
+    {
+        List<Alumno> alumnos = new List<Alumno>();
+        public ListView()
+        {
+            InitializeComponent();
+
+            alumnos.Add(new Alumno("Juan","Perez",30));
+            alumnos.Add(new Alumno("Carlos","Aguilar",40));
+
+            lvAlumnos.ItemsSource = alumnos;
+        }
+    
+    }
+}
