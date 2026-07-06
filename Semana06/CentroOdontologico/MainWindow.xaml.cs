0168153388221b7daefa63c056013551143f058f
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-07-06 18:11:21 -0500

    Centro odontologico completo

--- Semana06/CentroOdontologico/MainWindow.xaml.cs
+++ Semana06/CentroOdontologico/MainWindow.xaml.cs
@@ -19,6 +19,36 @@ namespace CentroOdontologico
         public MainWindow()
         {
             InitializeComponent();
+            calCita.SelectedDate = DateTime.Today;
+        }
+
+        private void btnCronograma_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPaciente.Text))
+            {
+                MessageBox.Show("Ingrese un paciente válido","Validar Paciente",MessageBoxButton.OK,MessageBoxImage.Warning);
+                return;
+            }
+
+            string paciente = txtPaciente.Text;
+
+            string tratamiento = ((ComboBoxItem)cbxTratamiento.SelectedItem).Content.ToString();
+            string pieza = ((ComboBoxItem)cbxPiezaDental.SelectedItem).Content.ToString();
+
+            DateTime fechaCita = calCita.SelectedDate.Value;
+
+            DateTime citaProxima = fechaCita.AddDays(15);
+
+            string reporte = $"Reporte de Cita\n" +
+                "========================\n"+
+                $"Paciente: {paciente}\n" +
+                $"Tratamiento: {tratamiento}\n" +
+                $"Pieza Dental: {pieza}\n" +
+                $"Fecha de Cita: {fechaCita.ToLongDateString()}\n" +
+                "------------------------\n"+
+                $"Fecha Proxima cita: {citaProxima.ToShortDateString()}";
+
+            txtReporte.Text = reporte;
         }
     }
 }
