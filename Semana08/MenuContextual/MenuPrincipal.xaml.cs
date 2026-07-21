04d23391790f8f3ce1e7708e990527f801bc0eaf
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-07-20 19:58:44 -0500

    Ejemplos Controles Contextuales

---
+++ Semana08/MenuContextual/MenuPrincipal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MenuContextual
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CommandBinding_Nuevo(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+
+        private void CommandBinding_Abrir(object sender, CanExecuteRoutedEventArgs e)
+        {
+            
+        }
+    }
+}
