32311e315f63bba25a11b5a3902d46e332f918ca
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-08-17 19:06:35 -0500

    Ejemplo MVVM

---
+++ Semana11/EjemploMVVM/Repositories/ProductoRepositoryImpl.cs
@@ -0,0 +1,40 @@
+﻿using EjemploMVVM.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace EjemploMVVM.Repositories
+{
+    public class ProductoRepositoryImpl : IProductoRepository
+    {
+        public List<Producto> ObtenerTodos()
+        {
+            string cn = "Server=.;Database=Northwind;Integrated Security=True;TrustServerCertificate=True";
+
+            using (SqlConnection conex = new SqlConnection(cn))
+            {
+                conex.Open();
+                string query = "SELECT ProductID,ProductName,UnitPrice,Discontinued From Products";
+
+                SqlCommand command = new SqlCommand(query, conex);
+
+                SqlDataReader reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                List<Producto> listaProductos = new List<Producto>();
+                while (reader.Read())
+                {
+                    Producto producto = new Producto
+                    {
+                        Id = reader.GetInt32(0),
+                        nombre=reader.GetString(1),
+                        precio = reader.GetDecimal(2),
+                        discontinuado = reader.GetBoolean(3)
+                    };
+                    listaProductos.Add(producto);
+                }
+                return listaProductos;
+            }
+        }
+    }
+}
---
+++ Semana11/EjemploMVVM/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using EjemploMVVM.Commands;
+using EjemploMVVM.Models;
+using EjemploMVVM.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EjemploMVVM.ViewModels
+{
+    public class ProductoViewModel
+    {
+        public ObservableCollection<Producto> productos { set; get; } = new ObservableCollection<Producto>();
+        public RelayCommand CargarProductosCommand { get; set; }
+
+        private IProductoRepository _repository;
+        public ProductoViewModel()
+        {
+            _repository = new ProductoRepositoryImpl();
+            CargarProductosCommand = new RelayCommand(CargarProductos);
+        }
+
+        private void CargarProductos()
+        {
+            List<Producto> lista = _repository.ObtenerTodos();
+            productos.Clear();
+            foreach (Producto producto in lista)
+            {
+                productos.Add(producto);
+            }
+        }
+
+    }
+}
